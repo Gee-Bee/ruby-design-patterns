3360d3144761b6052e91dfc56b3a3e45bd072914
Gee Bee <greg.bunia@gmail.com>
2014-10-01 21:28:37 +0200

    BS: Block code as Strategy

--- behavioral/strategy_test.rb
+++ behavioral/strategy_test.rb
@@ -25,13 +25,19 @@ class HeroTest < MiniTest::Test
       @hero = ::Hero.new ::BattleStats.new
     end
 
-    def test_battle_stats_printingn
+    def test_battle_stats_printing
       assert_equal "Damage: 10\nHealth: 5", @hero.print_stats
     end
 
     def test_skills_priting
       @hero.printer = ::SkillsStats.new
       assert_equal "Stealth\nDriving\nIntimidation\n", @hero.print_stats
+    end
+
+    def test_custom_ad_hoc_printer
+      assert_equal "Damage: 10\nNumber of skills: 3", (@hero.print_stats do   |damage, health, skills|
+        "Damage: #{damage}\nNumber of skills: #{skills.size}"
+      end)
     end
   end
 end
